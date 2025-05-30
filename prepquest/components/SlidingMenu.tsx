a397dcd0a440b77bfb2c599c03765f3f09c585e8
timtheteh <76463517+timtheteh@users.noreply.github.com>
2025-05-30 22:33:03 +0800

    sliding menu content

--- prepquest/components/SlidingMenu.tsx
+++ prepquest/components/SlidingMenu.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Animated, View, Platform, Dimensions } from 'react-native';
+import { StyleSheet, Animated, View, Platform, Dimensions, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 interface SlidingMenuProps {
   visible: boolean;
@@ -24,6 +26,16 @@ export function SlidingMenu({
         }
       ]}
     >
+      <View style={styles.menuContent}>
+        <View style={styles.menuItem}>
+          <FontAwesome name="folder" size={30} color="white" />
+          <Text style={styles.menuText}>View Folders</Text>
+        </View>
+        <View style={[styles.menuItem, styles.secondItem]}>
+          <FontAwesome name="star" size={30} color="white" />
+          <Text style={styles.menuText}>View Favorites</Text>
+        </View>
+      </View>
     </Animated.View>
   );
 }
@@ -39,5 +51,24 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 20,
     borderBottomRightRadius: 20,
     zIndex: 1001, // Higher than the grey overlay
+  },
+  menuContent: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    left: 15
+  },
+  menuItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
+  },
+  secondItem: {
+    marginTop: 33,
+  },
+  menuText: {
+    color: 'white',
+    fontSize: 16,
+    fontFamily: 'Satoshi-Medium',
   },
 }); 

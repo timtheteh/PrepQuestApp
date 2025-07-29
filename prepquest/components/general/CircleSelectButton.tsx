da231ce876c66f45372a94e3febc4eab5ddbefd6
timtheteh <76463517+timtheteh@users.noreply.github.com>
2025-07-29 20:48:10 +0800

    circleselectbutton optimized

--- prepquest/components/general/CircleSelectButton.tsx
+++ prepquest/components/general/CircleSelectButton.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useMemo, useCallback } from 'react';
 import { View, StyleSheet, ViewStyle, TouchableOpacity, Animated } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import { useTheme } from '@/contexts/ThemeContext';
+import { Colors } from '@/constants/Colors';
 
 interface CircleSelectButtonProps {
   style?: ViewStyle;
@@ -10,38 +12,52 @@ interface CircleSelectButtonProps {
   opacity?: Animated.Value;
 }
 
-export function CircleSelectButton({ 
+export const CircleSelectButton = React.memo(({ 
   style,
   selected = false,
   onPress,
   disabled = false,
   opacity
-}: CircleSelectButtonProps) {
+}: CircleSelectButtonProps) => {
+  const { theme } = useTheme();
+  const colors = Colors[theme];
+  const styles = createStyles(colors);
+  
+  const animatedViewStyle = useMemo(() => [
+    styles.container,
+    opacity !== undefined && { opacity },
+    style
+  ], [styles.container, opacity, style]);
+  
+  const circleStyle = useMemo(() => [
+    styles.circle,
+    selected && styles.selected
+  ], [styles.circle, selected, styles.selected]);
+  
+  const handlePress = useCallback(() => {
+    if (onPress) {
+      onPress();
+    }
+  }, [onPress]);
+  
   return (
-    <Animated.View style={[
-      styles.container,
-      opacity !== undefined && { opacity },
-      style
-    ]}>
+    <Animated.View style={animatedViewStyle}>
       <TouchableOpacity
-        onPress={onPress}
+        onPress={handlePress}
         disabled={disabled}
         style={styles.button}
       >
-        <View style={[
-          styles.circle,
-          selected && styles.selected
-        ]}>
+        <View style={circleStyle}>
           {selected && (
-            <Feather name="check" size={18} color="white" />
+            <Feather name="check" size={18} color={colors.background} />
           )}
         </View>
       </TouchableOpacity>
     </Animated.View>
   );
-}
+});
 
-const styles = StyleSheet.create({
+const createStyles = (colors: any) => StyleSheet.create({
   container: {
     width: 30,
     height: 30,
@@ -54,13 +70,13 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     borderRadius: 15,
-    backgroundColor: 'white',
+    backgroundColor: colors.background,
     borderWidth: 3,
-    borderColor: '#4F41D8',
+    borderColor: colors.brandColor2,
     alignItems: 'center',
     justifyContent: 'center',
   },
   selected: {
-    backgroundColor: '#4F41D8',
+    backgroundColor: colors.brandColor2,
   },
 }); 

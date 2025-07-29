01184ae91221cbe599f6591b8ee58227515e56b2
timtheteh <76463517+timtheteh@users.noreply.github.com>
2025-07-29 20:54:48 +0800

    CircleSVGIconButton optimized

--- prepquest/components/general/CircleSVGIconButton.tsx
+++ prepquest/components/general/CircleSVGIconButton.tsx
@@ -1,5 +1,8 @@
+import React, { useMemo, useCallback } from 'react';
 import { StyleSheet, TouchableOpacity, ViewStyle } from 'react-native';
 import { SvgProps } from 'react-native-svg';
+import { useTheme } from '@/contexts/ThemeContext';
+import { Colors } from '@/constants/Colors';
 
 interface CircleSVGIconButtonProps {
   Icon: React.FC<SvgProps>;
@@ -9,30 +12,45 @@ interface CircleSVGIconButtonProps {
   style?: ViewStyle;
 }
 
-export function CircleSVGIconButton({ 
+export const CircleSVGIconButton = React.memo(({ 
   Icon, 
   size = 24,
   onPress,
   color = 'black',
   style
-}: CircleSVGIconButtonProps) {
+}: CircleSVGIconButtonProps) => {
+  const { theme } = useTheme();
+  const colors = Colors[theme];
+  const styles = createStyles(colors);
+  
+  const buttonStyle = useMemo(() => [
+    styles.circleButton, 
+    style
+  ], [styles.circleButton, style]);
+  
+  const handlePress = useCallback(() => {
+    if (onPress) {
+      onPress();
+    }
+  }, [onPress]);
+  
   return (
     <TouchableOpacity 
-      style={[styles.circleButton, style]}
+      style={buttonStyle}
       activeOpacity={1}
-      onPress={onPress}
+      onPress={handlePress}
     >
       <Icon width={size} height={size} color={color} />
     </TouchableOpacity>
   );
-}
+});
 
-const styles = StyleSheet.create({
+const createStyles = (colors: any) => StyleSheet.create({
   circleButton: {
     width: 46,
     height: 46,
     borderRadius: 23,
-    backgroundColor: '#F8F8F8',
+    backgroundColor: colors.secondaryShade,
     justifyContent: 'center',
     alignItems: 'center',
   }
